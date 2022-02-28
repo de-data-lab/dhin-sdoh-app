6fa2cc9869f6090e92f982c87cc884197e7bb0c9
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-28 16:20:32 -0500

    Add a simple homepage

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, Response, jsonify, make_response, request
+from urllib.parse import urlparse
 import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
 
 dhin_cwi_df = pd.read_csv('data/dhin-cwi.csv')
+
+@app.route('/')
+def home():
+    current_url = urlparse(request.base_url)
+    entry_point = current_url.hostname + '/data'
+
+    return 'Use the API entrypoint ' + entry_point + ' to query'
 
 @app.route('/data', methods = ['GET'])
 def get_data():
